7e3b32c29147532acc171eebe3a108e906eea6e1
Rosie O'Donnell <rosie.odonnell@talk21.com>
2021-03-27 13:50:47 +0000

    setting up landing page and creating initial links to other pages

--- h4h-team7-web/components/index.ts
+++ h4h-team7-web/components/index.ts
@@ -2,3 +2,4 @@
 export { RegistrationForm } from './registration-form/registration-form.component';
 export { Title } from './title/title.component';
 export { Header } from './header/header.component';
+export { BackgroundImage } from './background-image/background-image.component';
--- h4h-team7-web/pages/index.tsx
+++ h4h-team7-web/pages/index.tsx
@@ -5,7 +5,22 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <BaseLayout>
-      Test
+    {/* <link href="https://fonts.googleapis.com/css?family=Abhaya+Libre:400,800|Montserrat:600|Alegreya+Sans:500&display=swap" rel="stylesheet">
+  */}
+<main className="bg-white font-abhaya-libre relative">
+  
+    <div className="container mx-auto px-6 sm:px-12 flex flex-col sm:flex-row items-center relative z-10">
+        <div className="sm:w-1/2 xl:w-2/6 flex flex-col items-start py-24 sm:py-0">
+            <h1 className="text-6xl xl:text-10xl font-abhaya-libre text-green-900 font-bold leading-none">Nature.</h1>
+            <h2 className="text-xl xl:text-3xl font-abhaya-libre text-green-900 uppercase font-bold leading-none tracking-widest -mt-2 mb-6">Landing page</h2>
+            <p className="xl:text-lg tracking-wider text-gray-700 font-alegraya-sans">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh  minim veniam, quis nostrud</p>
+            <a href="#" className="font-montserrat text-white sm:font-xl uppercase py-3 px-6 sm:py-4 sm:px-8 rounded-full shadow-lg bg-green-900 hover:bg-green-800 mt-8 sm:mt-16">Read more</a>
+        </div>
+        <div className="hidden sm:flex items-center justify-center xl:justify-end w-1/2 xl:w-4/6 py-32">
+           </div>
+    </div>
+    
+</main>
     </BaseLayout>
   )
 }
