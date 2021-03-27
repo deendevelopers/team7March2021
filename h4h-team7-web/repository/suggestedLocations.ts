a560cc372802b9600a94f601737fb417ad5cd13d
Rees, Dougal <dorees@deloitte.co.uk>
2021-03-27 13:40:00 +0000

    add data repository

---
+++ h4h-team7-web/repository/suggestedLocations.ts
@@ -0,0 +1,14 @@
+export const suggestedLocations = [
+    {
+        name:"Clapham Common Band Stand",
+        postcode:"SW4 0AA"
+    },
+    {
+        name:"Clapham library",
+        postcode:"SW4 0AA"
+    },
+    {
+        name:"Brickwood Cafe",
+        postcode:"SW4 0AA"
+    }
+]
