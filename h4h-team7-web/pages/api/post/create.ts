cd929fa55fa2195894290ce6f074be11e7494a87
alan.hutcheson <alanhutcheson@deloitte.co.uk>
2021-03-27 14:48:48 +0000

    fixing api requests

--- h4h-team7-web/pages/api/post/create.ts
+++ h4h-team7-web/pages/api/post/create.ts
@@ -4,5 +4,5 @@ import PostRepository from '../../../repository/PostRepository'
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { post } = JSON.parse(req.body)
   PostRepository.createPost(post);
-  res.status(201);
+  res.status(201).end();
 }
